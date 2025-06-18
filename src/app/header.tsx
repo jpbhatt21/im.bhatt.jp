a9c73eb08a0cc88d21507af432e6dc80b57b8137
Jatan <jatanpbhatt@gmail.com>
2025-06-18 18:08:44 +0530

    on scroll anim

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -15,11 +15,13 @@ function Header() {
 			const element = ref.current;
 			const lastChild= element.lastElementChild as HTMLDivElement;
 			cur = window.scrollY;
+			let main = document.querySelector("main");
 			if (cur > prev) {
 				element.style.top = "-12px";
 				element.style.opacity = "0";
 				element.style.filter = "blur(4px)";
 				lastChild.style.width = "0px";
+			
 
 			} else {
 				element.style.top = "0px";
@@ -31,15 +33,19 @@ function Header() {
 		});
 	}, []);
 	return (
-		<div ref={ref} className="fixed lg:left-1/2 duration-500 z-20 lg:w-xl lg:-translate-x-1/2  w-full top-0  bg- ado/25 backdrop-blur gap-2 text-oxford-blue-900 border-dark-purple-500 bo rder h-16 items-center flex flex-col">
+		<div ref={ref} className="fixed lg:left-1/2 duration-500 z-20 lg:w-[40vw] lg:-translate-x-1/2  w-full top-0  bg- ado/25 backdrop-blur gap-2 text-oxford-blue-900 border-dark-purple-500 bo rder h-[8vh] items-center flex flex-col">
 			<div className="w-full h-full px-4 flex items-center ">
 				<label className="eng text-2xl flex hidden">
 				j<span className=" p">p</span>
 				<span className=" b">b</span>
 			</label>
-			<label className={"eng text-ado-blue-800 text-2xl "+germ.className}>jpb</label>
+			<label className={"eng text-ado-blue-800 flex text-[3vh] "+germ.className}>
+
+				<span className="">j</span>
+				<span className="-ml-[0.7vh] mt-[0.6vh]">b</span>
+			</label>
 			</div>
-			<div className="w-full duration-300 bg-ado-blue-600 h-[1px]"></div>
+			<div className="w-full duration-300 bg-ado-blue-600 h-[0.1vmin]"></div>
 		</div>
 	);
 }
